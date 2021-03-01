64bbedcf9c35bdd5d890801f9b67fc32122eb960
Aman Patel <patel1wq@uwindsor.ca>
2021-03-01 09:30:48 -0500

    updated footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,15 +55,12 @@ module.exports = {
           ],
         },
         {
+          /* TDB: Replace this with something else*/
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
               label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              href: 'https://css.uwindsor.ca/discord',
             },
             {
               label: 'Twitter',
@@ -72,20 +69,29 @@ module.exports = {
           ],
         },
         {
-          title: 'More',
+          title: 'Socials',
           items: [
             {
-              label: 'Blog',
-              to: 'blog',
+              label: 'LinkedIn',
+              href: 'https://www.linkedin.com/company/uwindsorcss/',
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/UWindsorCSS/Wiki',
+              label: 'Twitter',
+              href: 'https://twitter.com/UWindsorCSS',
             },
+            {
+              label: 'Instagram',
+              href: 'https://www.instagram.com/uwindsorcss/',
+            },
+            {
+              label: 'facebook',
+              href: 'https://www.facebook.com/uwindsorcss',
+            },
+
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Computer Science Society, Built with Docusaurus.`,
     },
   },
   presets: [
