72ff975cc76c9cb30996bda0b2b98e2182f0fd10
Ryan Prairie <me@ryanprairie.com>
2021-03-27 13:45:28 -0400

    made a dummy sidebar for resources

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -174,7 +174,7 @@ module.exports = {
       {
           id: 'plugin-docs-resources',
           path: 'resources',
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve('./resources/resources_sidebar.js'),
           routeBasePath: 'resources',
           editUrl:
             'https://github.com/UWindsorCSS/Wiki/tree/master/',
---
+++ resources/resources_sidebar.js
@@ -0,0 +1,6 @@
+module.exports = {
+	indexNav: {
+		SiteMap: ['index'],
+		StyleGuide: ['style_guide'],
+	}
+};
