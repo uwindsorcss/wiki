b0541da68381a73473fe53f76a2bce6806add819
jere-mie <jere-mie@users.noreply.github.com>
2022-03-19 11:47:15 -0400

    add 2140 and 2660 guides

--- courses/courses_sidebars.js
+++ courses/courses_sidebars.js
@@ -4,5 +4,20 @@ module.exports = {
             type: "doc",
             id: "Offered Courses",
         },
+        {
+            type: "category",
+            label: "COMP-2140",
+            items: [
+                "COMP-2140/installingJLex",
+                "COMP-2140/installingJavaCup",
+            ],
+        },
+        {
+            type: "category",
+            label: "COMP-2660",
+            items: [
+                "COMP-2660/easyMASM",
+            ],
+        },
     ],
 };
