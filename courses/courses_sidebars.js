1c03c3f3f1dca2a46f51c41e3010704836c36741
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2022-07-17 13:15:55 -0400

    Added dynamic table scripts

--- courses/courses_sidebars.js
+++ courses/courses_sidebars.js
@@ -2,22 +2,17 @@ module.exports = {
     indexNav: [
         {
             type: "doc",
-            id: "Offered Courses",
+            id: "offering/Offered Courses",
         },
         {
             type: "category",
             label: "COMP-2140",
-            items: [
-                "COMP-2140/installingJLex",
-                "COMP-2140/installingJavaCup",
-            ],
+            items: ["COMP-2140/installingJLex", "COMP-2140/installingJavaCup"],
         },
         {
             type: "category",
             label: "COMP-2660",
-            items: [
-                "COMP-2660/easyMASM",
-            ],
+            items: ["COMP-2660/easyMASM"],
         },
     ],
 };
