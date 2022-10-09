3fd0a06a6c4b9787e124c4e26ed3b312093ae72a
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2022-10-09 16:17:34 -0400

    Update courses_sidebars.js

--- courses/courses_sidebars.js
+++ courses/courses_sidebars.js
@@ -13,85 +13,314 @@ module.exports = {
                 "course_planning/specialcourse",
             ],
         },
-
         {
             type: "category",
-            label: "COMP-1000",
-            items: ["COMP-1000/overview"],
-        },
-        {
-            type: "category",
-            label: "COMP-1400",
-            items: ["COMP-1400/overview"],
-        },
-        {
-            type: "category",
-            label: "COMP-1410",
-            items: ["COMP-1410/overview"],
-        },
-        {
-            type: "category",
-            label: "COMP-2120",
-            items: ["COMP-2120/overview"],
-        },
-        {
-            type: "category",
-            label: "COMP-2140",
+            label: "Year 1 Computer Science Courses",
             items: [
-                "COMP-2140/overview",
-                "COMP-2140/installingJLex",
-                "COMP-2140/installingJavaCup",
+                {
+                    type: "category",
+                    label: "COMP-1000",
+                    items: ["COMP-1000/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-1047",
+                    items: ["COMP-1047/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-1400",
+                    items: ["COMP-1400/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-1410",
+                    items: ["COMP-1410/overview"],
+                },
             ],
         },
         {
             type: "category",
-            label: "COMP-2310",
-            items: ["COMP-2310/overview", "COMP-2310/surviving2310"],
+            label: "Year 2 Computer Science Courses",
+            items: [
+                {
+                    type: "category",
+                    label: "COMP-2057",
+                    items: ["COMP-2057/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-2067",
+                    items: ["COMP-2067/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-2077",
+                    items: ["COMP-2077/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-2097",
+                    items: ["COMP-2097/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-2120",
+                    items: ["COMP-2120/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-2140",
+                    items: [
+                        "COMP-2140/overview",
+                        "COMP-2140/installingJLex",
+                        "COMP-2140/installingJavaCup",
+                    ],
+                },
+                {
+                    type: "category",
+                    label: "COMP-2310",
+                    items: ["COMP-2310/overview", "COMP-2310/surviving2310"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-2540",
+                    items: ["COMP-2540/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-2560",
+                    items: ["COMP-2560/overview", "COMP-2560/advice"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-2650",
+                    items: ["COMP-2650/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-2660",
+                    items: ["COMP-2660/overview", "COMP-2660/easyMASM"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-2707",
+                    items: ["COMP-2707/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-2750",
+                    items: ["COMP-2750/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-2800",
+                    items: ["COMP-2800/overview"],
+                },
+            ],
         },
         {
             type: "category",
-            label: "COMP-2540",
-            items: ["COMP-2540/overview"],
+            label: "Year 3 Computer Science Courses",
+            items: [
+                {
+                    type: "category",
+                    label: "COMP-3057",
+                    items: ["COMP-3057/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-3077",
+                    items: ["COMP-3077/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-3110",
+                    items: ["COMP-3110/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-3150",
+                    items: ["COMP-3150/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-3220",
+                    items: ["COMP-3220/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-3300",
+                    items: ["COMP-3300/overview", "COMP-3300/advice"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-3340",
+                    items: ["COMP-3340/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-3400",
+                    items: ["COMP-3400/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-3500",
+                    items: ["COMP-3500/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-3520",
+                    items: ["COMP-3520/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-3540",
+                    items: ["COMP-3540/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-3670",
+                    items: ["COMP-3670/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-3680",
+                    items: ["COMP-3680/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-3710",
+                    items: ["COMP-3710/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-3770",
+                    items: ["COMP-3770/overview"],
+                },
+            ],
         },
         {
             type: "category",
-            label: "COMP-2560",
-            items: ["COMP-2560/overview", "COMP-2560/advice"],
+            label: "Year 4 Computer Science Courses",
+            items: [
+                {
+                    type: "category",
+                    label: "COMP-4110",
+                    items: ["COMP-4110/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-4150",
+                    items: ["COMP-4150/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-4200",
+                    items: ["COMP-4200/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-4220",
+                    items: ["COMP-4220/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-4250",
+                    items: ["COMP-4250/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-4400",
+                    items: ["COMP-4400/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-4500",
+                    items: ["COMP-4500/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-4540",
+                    items: ["COMP-4540/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-4670",
+                    items: ["COMP-4670/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-4680",
+                    items: ["COMP-4680/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-4730",
+                    items: ["COMP-4730/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-4740",
+                    items: ["COMP-4740/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-4770",
+                    items: ["COMP-4770/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-4800",
+                    items: ["COMP-4800/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-4960",
+                    items: ["COMP-4960/overview"],
+                },
+                {
+                    type: "category",
+                    label: "COMP-4990",
+                    items: ["COMP-4990/overview"],
+                },
+            ],
         },
         {
             type: "category",
-            label: "COMP-2650",
-            items: ["COMP-2650/overview"],
-        },
-        {
-            type: "category",
-            label: "COMP-2660",
-            items: ["COMP-2660/overview", "COMP-2660/easyMASM"],
-        },
-        {
-            type: "category",
-            label: "COMP-2800",
-            items: ["COMP-2800/overview"],
-        },
-        {
-            type: "category",
-            label: "COMP-3110",
-            items: ["COMP-3110/overview"],
-        },
-        {
-            type: "category",
-            label: "COMP-3150",
-            items: ["COMP-3150/overview"],
-        },
-        {
-            type: "category",
-            label: "COMP-3220",
-            items: ["COMP-3220/overview"],
-        },
-        {
-            type: "category",
-            label: "COMP-3300",
-            items: ["COMP-3300/overview", "COMP-3300/advice"],
+            label: "Math and Stats Courses",
+            items: [
+                {
+                    type: "category",
+                    label: "MATH-1020",
+                    items: ["MATH-1020/overview"],
+                },
+                {
+                    type: "category",
+                    label: "MATH-1250",
+                    items: ["MATH-1250/overview"],
+                },
+                {
+                    type: "category",
+                    label: "MATH-1720",
+                    items: ["MATH-1720/overview"],
+                },
+                {
+                    type: "category",
+                    label: "MATH-1730",
+                    items: ["MATH-1730/overview"],
+                },
+                {
+                    type: "category",
+                    label: "MATH-3940",
+                    items: ["MATH-3940/overview"],
+                },
+                {
+                    type: "category",
+                    label: "STAT-2910",
+                    items: ["STAT-2910/overview"],
+                },
+            ],
         },
     ],
 };
