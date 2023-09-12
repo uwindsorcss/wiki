ad277ea51473804e752dd501d773a64d4c6fea5f
Borhan Saflo <60056206+BorhanSaflo@users.noreply.github.com>
2023-09-12 01:33:04 -0400

    fix: Navigation Guide Footer Link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -98,7 +98,7 @@ module.exports = {
                         },
                         {
                             label: "How to Navigate the Wiki",
-                            to: "resources/guides/Navigation Guide/navigation-guide",
+                            to: "resources/guides/navigation-guide/navigation-guide",
                         },
                     ],
                 },
